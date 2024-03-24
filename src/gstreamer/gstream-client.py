d560523fd3f51297b302f729aea5e109ee43dd20
Jonathan Blixt <jmblixt3@gmail.com>
2024-03-23 19:13:50 -0500

    Updates to client

--- src/gstreamer/gstream-client.py
+++ src/gstreamer/gstream-client.py
@@ -1,54 +1,29 @@
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import GLib, Gst, GObject
-import sys
-import signal
+
+from threading import Thread, Event
 import paramiko
-import threading
-import socket
 from pyroute2 import IPRoute
-
-def sigint_handler(sig, frame):
-    print("\n Exiting\n ")
-    server.close()
-    pipeline.set_state(Gst.State.NULL)
-    sys.exit(0)
-
+import keyboard
+import sys 
 # Initialize GStreamer
 Gst.init(None)
 print("Creating Pipeline \n ")
 pipeline = Gst.Pipeline()
-signal.signal(signal.SIGINT, sigint_handler)
-
-if not pipeline:
-    print(" Unable to create Pipeline \n")
 
 source = Gst.ElementFactory.make("udpsrc", "src")
-if not source:
-    print(" Unable to create Source \n")
+source.set_property("port", 5000)
 
 caps_v4l2src = Gst.ElementFactory.make("capsfilter", "v4l2src_caps")
-
-if not caps_v4l2src:
-    print(" Unable to create v4l2src capsfilter \n")
+caps_v4l2src.set_property('caps', Gst.Caps.from_string("video/x-av1,width=640,height=480,framerate=15/1"))
 
 queue = Gst.ElementFactory.make("queue", "queue")
 
-if not queue:
-    print(" Unable to create Queue \n")
-
 decoder = Gst.ElementFactory.make("nvv4l2decoder", "decoder")
 
-if not decoder:
-    print(" Unable to create Decoder \n")
+sink = Gst.ElementFactory.make("autovideosink", "sink")
 
-sink = Gst.ElementFactory.make("nveglglessink", "sink")
-
-if not sink:
-    print(" Unable to create Sink \n")
-
-source.set_property("port", 5000)
-caps_v4l2src.set_property('caps', Gst.Caps.from_string("video/x-av1,width=640,height=480,framerate=15/1"))
 
 print("Adding srcs \n")
 pipeline.add(source)
@@ -56,6 +31,7 @@ pipeline.add(caps_v4l2src)
 pipeline.add(queue)
 pipeline.add(decoder)
 pipeline.add(sink)
+
 print("Linking srcs \n")
 #link the elements
 source.link(caps_v4l2src)
@@ -67,39 +43,49 @@ decoder.link(sink)
 print("Starting pipeline \n")
 pipeline.set_state(Gst.State.PLAYING)
 
+stop_event = Event()
+change_source_thread = Thread()
+change_source_thread.start()
 
 server = paramiko.SSHClient()
 key = paramiko.RSAKey.from_private_key_file("/root/.ssh/id_orin")
 server.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 remote = "10.133.240.45" #need to make this dynamic
-user="rovr-orin"
+user="blixt"
 # get current program ip
 with IPRoute() as ipr:
     client = ipr.route('get', dst=remote)[0].get_attr('RTA_PREFSRC')
 
 server.connect(hostname=remote,username=user,pkey=key)
 server.get_transport().set_keepalive(1)
-cmd = f'gst-launch-1.0 videotestsrc ! "video/x-raw,width=640,height=480,framerate=15/1" ! nvvidconv ! "video/x-raw(memory:NVMM),format=NV12" ! nvv4l2av1enc bitrate=200000 ! "video/x-av1" ! udpsink host={client} port=5000'
+cmd = f'python3 Lunabotics-2024/src/gstreamer/gstreamer-server.py {client}'
 print(cmd)
-stdin, stdout, stderr = server.exec_command(cmd)
-# def line_buffered(f):
-#     line_buf = ""
-#     while not f.channel.exit_status_ready():
-#         line_buf += f.read(1)
-#         if line_buf.endswith('\n'):
-#             yield line_buf
-#             line_buf = ''
+stdin, stdout, stderr = server.exec_command(cmd,get_pty=True)
+server.get_transport().set_keepalive(1)
+def line_buffered(f):
+    line_buf = ""
+    while not f.channel.exit_status_ready():
+        line_buf += f.read(1)
+        if line_buf.endswith('\n'):
+            yield line_buf
+            line_buf = ''
 
-# for l in line_buffered(stdout):
-#     print (l)
-
-# stdout_thread = threading.Thread(target=(lambda stdout: sys.stdout.write(stdout.read())), args=(stdout,))
-# stderr_thread = threading.Thread(target=(lambda stderr: sys.stderr.write(stderr.read())), args=(stderr,))
-# stdout_thread.start()
-# stderr_thread.start()
 # Run the main loop
-print("Running loop \n")
-loop = GLib.MainLoop()
-loop.run()
-
-print("Should never read here\n")
+print("Running loop")
+while True:
+    try:
+        message:Gst.Message = pipeline.get_bus().timed_pop(Gst.SECOND)
+        if message == None:
+            pass
+        elif message.type == Gst.MessageType.EOS:
+            break
+        elif message.type == Gst.MessageType.ERROR:
+            gi.error("Error", message.parse_error())
+            break
+    except KeyboardInterrupt:
+        break
+print("Exiting")
+stop_event.set()
+pipeline.set_state(Gst.State.NULL)
+server.close()
+print("Exited")
