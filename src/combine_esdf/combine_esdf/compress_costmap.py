607c778e5003ebd929d4c15f2ed710002ab22374
alex berg <aaberg333@gmail.com>
2024-04-17 20:13:23 -0500

    kind of works. ~2 kilobytes per image i think.

--- src/combine_esdf/combine_esdf/compress_costmap.py
+++ src/combine_esdf/combine_esdf/compress_costmap.py
@@ -2,9 +2,10 @@ import rclpy
 from rclpy.node import Node
 
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
-
 from nvblox_msgs.msg import DistanceMapSlice
+import sys
 import numpy as np
+from PIL import Image
 
 class compress_costmap(Node):
     def __init__(self):
@@ -12,24 +13,48 @@ class compress_costmap(Node):
         
         self.nav2 = BasicNavigator()
 
-        # self.nav2.waitUntilNav2Active()
-
-        print(self.nav2.getFeedback())
-
-        # self.costmap_subscriber = self.create_subscription(
-        #     DistanceMapSlice,
-        #     '/combined_esdf',
-        #     self.costmap_callback,
-        #     10)
+       
         
-        # self.costmap = None
-        
-        # self.timer = self.create_timer(0.5, self.publish_costmap)
+        self.timer = self.create_timer(0.5, self.publish_costmap)
 
         # self.costmap_publisher = self.create_publisher(DistanceMapSlice, '/compressed_esdf', 10)
 
     def costmap_callback(self, msg):
         self.costmap = msg
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def publish_costmap(self):
+        costmap = self.nav2.getGlobalCostmap()
+        format = costmap.metadata
+        res = format.resolution
+        # print(sys.getsizeof(costmap.data))
+        data = np.array(costmap.data).reshape((format.size_x, format.size_y))
+        # print(data.shape)
+        
+        data = data[900:1100, 900:1100]
+        data[data == 0] = 255
+        # data = data[::4, ::4]
+        # data = data.astype('uint8')
+        # print(sys.getsizeof(data.flatten().tolist))
+        print(sys.getsizeof(data.tolist()))
+        print(type(data.flatten()))
+        image = Image.fromarray(data.astype('uint8'))
+        # Save the image
+        image.save('matrix_image.png', quality=1)
+        image2 = Image.open("matrix_image.png").convert("L")
+        img_matrix = np.array(image2)
+        print(sys.getsizeof(img_matrix.flatten()))
     
 
 
